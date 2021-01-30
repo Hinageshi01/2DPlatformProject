40932c248dad5dbbde6f389a78f5a70469170162
Hinageshi <2247001665@qq.com>
2021-01-30 12:07:36 +0800

    实现基本动作的动画

--- 2DPlatformProject/Assets/Scripts/PlayerController.cs
+++ 2DPlatformProject/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D body;
     public float speed;
     public float jumpForce;
+    public Animator animator;
+    public Collider2D collider2d;
+    public LayerMask ground;
 
     void Start()
     {
@@ -16,17 +19,32 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+        AnimationSwitch();
     }
 
     void Movement() {
         float horizontalMove = Input.GetAxis("Horizontal");
         float faceDirection = Input.GetAxisRaw("Horizontal");
-            body.velocity = new Vector2(horizontalMove * speed, body.velocity.y);
+        animator.SetFloat("Running", Mathf.Abs(horizontalMove));
+        body.velocity = new Vector2(horizontalMove * speed, body.velocity.y);//移动
         if (faceDirection != 0) {
-            transform.localScale = new Vector3(faceDirection, 1, 1);
+            transform.localScale = new Vector3(faceDirection, 1, 1);//转身
         }
         if (Input.GetButtonDown("Jump")) {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            body.velocity = new Vector2(body.velocity.x, jumpForce);//跳跃
+            animator.SetBool("Jumping", true);
+        }
+    }
+    void AnimationSwitch() {
+        if (animator.GetBool("Jumping")) {
+            if (body.velocity.y <= 0) {
+                animator.SetBool("Jumping", false);
+                animator.SetBool("Falling", true);
+            }
+        }
+        else if (collider2d.IsTouchingLayers(ground)){
+            animator.SetBool("Falling", false);
+            animator.SetBool("Idling", true);
         }
     }
 }
