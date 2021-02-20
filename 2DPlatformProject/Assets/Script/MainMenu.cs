81d252ea1871103327c57fbcf23f70ef46a3ba53
Hinageshi <2247001665@qq.com>
2021-02-20 23:47:24 +0800

    2/20

---
+++ 2DPlatformProject/Assets/Script/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject titleUI, startUI, endUI;
+    void Start() {
+        TitleAppear();
+    }
+    public void TitleAppear() {
+        StartCoroutine(Delayed(0.5f, titleUI, startUI, endUI));
+    }
+    IEnumerator Delayed(float delayTime, params GameObject[] list) {
+        foreach (GameObject obj in list) {
+            yield return new WaitForSeconds(delayTime);
+            SetActiveTrue(obj);
+        }
+    }
+    private void SetActiveTrue(GameObject obj) {
+        obj.SetActive(true);
+    }
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
---
+++ 2DPlatformProject/Assets/Script/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
+    public void PauseGame() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+    public void SetAudioVolume(float volume) {
+        audioMixer.SetFloat("MainVolume", volume);
+    }
+}
